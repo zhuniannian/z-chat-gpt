00476ee58fcb9da82a06b516f3eca64f774c3a25
zhuniannian <1506698691@qq.com>
2023-04-12 16:30:21 +0800

    Initial commit

---
+++ src/main/java/com/chatgpt/service/ChatService.java
@@ -0,0 +1,58 @@
+package com.chatgpt.service;
+
+import com.chatgpt.util.Chat;
+import com.unfbx.chatgpt.entity.chat.Message;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/chat")
+public class ChatService {
+
+    @Resource private Chat chat;
+
+    @Resource
+    RestTemplate restTemplate;
+
+    @PostMapping
+    public List<Message> getChat(@RequestBody String content) {
+        return chat.send(content);
+    }
+
+//    public static ExecutorService executors = Executors.newFixedThreadPool(15);
+//
+//    @GetMapping(value = "/get")
+//    public String get() {
+//        List<String> strings = new ArrayList<>();
+//        long cur = System.currentTimeMillis();
+//        List<CompletableFuture<String>> futures = new ArrayList<>();
+//        for (int i = 0; i < 700; i++) {
+//            futures.add(CompletableFuture.supplyAsync(() -> restTemplate.getForObject("http://localhost:8889/chat2", String.class), executors));
+//        }
+//        //等待全部完成
+//        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+//        futures.forEach(stringCompletableFuture -> {
+//            try {
+//                strings.add(stringCompletableFuture.get(3, TimeUnit.SECONDS));
+//            } catch (Exception e) {
+//                System.out.println(e.getMessage());
+//            }
+//        });
+//        System.out.println("end-----"+ (System.currentTimeMillis() - cur) / 1000);
+//        System.out.println(strings.size());
+//        return strings.toString();
+//    }
+//
+//
+//    @GetMapping(value = "/get2")
+//    public String get2() {
+//        return "hhh";
+//    }
+
+}
---
+++ src/main/java/com/chatgpt/util/Chat.java
@@ -0,0 +1,65 @@
+package com.chatgpt.util;
+
+import com.unfbx.chatgpt.OpenAiClient;
+import com.unfbx.chatgpt.entity.chat.ChatCompletion;
+import com.unfbx.chatgpt.entity.chat.ChatCompletionResponse;
+import com.unfbx.chatgpt.entity.chat.Message;
+import com.unfbx.chatgpt.interceptor.OpenAILogger;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import org.springframework.stereotype.Service;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Instruction GPT模型
+ */
+@Service
+public class Chat {
+
+    private static final OpenAiClient openAiClient;
+
+    static {
+        System.out.println("初始化chatgpt信息-------------------------");
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 7890));
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor(new OpenAILogger());
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient okHttpClient = new OkHttpClient
+                .Builder()
+                .proxy(proxy)//自定义代理
+                .addInterceptor(httpLoggingInterceptor)//自定义日志输出
+                .connectTimeout(30, TimeUnit.SECONDS)//自定义超时时间
+                .writeTimeout(3, TimeUnit.MINUTES)//自定义超时时间
+                .readTimeout(3, TimeUnit.MINUTES)//自定义超时时间
+                .build();
+        //构建客户端
+       openAiClient = OpenAiClient.builder()
+                .apiKey(Collections.singletonList("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"))
+                .okHttpClient(okHttpClient)
+                //自己做了代理就传代理地址，没有可不不传
+//                .apiHost("https://自己代理的服务器地址/")
+                .build();
+    }
+
+
+    public List<Message> send(String content) {
+        //聊天模型：gpt-3.5
+        List<Message> resp = new ArrayList<>();
+        Message message = Message.builder().role(Message.Role.USER).content(content).build();
+        ChatCompletion chatCompletion = ChatCompletion.builder().messages(Arrays.asList(message)).build();
+        ChatCompletionResponse chatCompletionResponse = openAiClient.chatCompletion(chatCompletion);
+        chatCompletionResponse.getChoices().forEach(e -> {
+            System.out.println(e.getMessage());
+            resp.add(e.getMessage());
+        });
+        return resp;
+    }
+
+
+}
